ec1909588984f11db3ced7e0a1d1379b28ae6067
youpi <youpila2@gmail.com>
2015-11-07 13:38:59 +0100

    Ajout de la classe phénomène

---
+++ Phenomenon.cpp
@@ -0,0 +1,21 @@
+/*
+ * Phenomenon.cpp
+ *
+ *  Created on: Nov 2, 2015
+ *      Author:
+ */
+
+#include "Phenomenon.h"
+
+namespace Phenomenon {
+
+Phenomenon() {
+	// TODO Auto-generated constructor stub
+
+}
+
+~Phenomenon() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Phenomenon */
---
+++ Phenomenon.h
@@ -0,0 +1,21 @@
+/*
+ * Phenomenon.h
+ *
+ *  Created on: Nov 2, 2015
+ *      Author:
+ */
+
+#ifndef PHENOMENON_H_
+#define PHENOMENON_H_
+
+namespace Phenomenon {
+
+class Phenomenon {
+public:
+	Phenomenon();
+	virtual ~Phenomenon();
+};
+
+} /* namespace Phenomenon */
+
+#endif /* PHENOMENON_H_ */
