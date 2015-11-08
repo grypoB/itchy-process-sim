05e30e207f60052e56782d53ea3981591df19331
youpi <youpila2@gmail.com>
2015-11-08 18:33:21 +0100

    Ajout de la classe Server

---
+++ Server.cpp
@@ -0,0 +1,21 @@
+/*
+ * Server.cpp
+ *
+ *  Created on: Nov 2, 2015
+ *      Author:
+ */
+
+#include "Server.h"
+
+namespace server {
+
+Server::Server() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Server::~Server() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace server */
---
+++ Server.h
@@ -0,0 +1,21 @@
+/*
+ * Server.h
+ *
+ *  Created on: Nov 2, 2015
+ *      Author:
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+namespace server {
+
+class Server {
+public:
+	Server();
+	virtual ~Server();
+};
+
+} /* namespace server */
+
+#endif /* SERVER_H_ */
