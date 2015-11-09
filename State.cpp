af34040a81cf4b48732a4e1d446d520f98388eb9
youpila <youpila2@gmail.com>
2015-11-09 11:24:07 +0100

    Mise a jour architecture : classe State

--- State.cpp
+++ State.cpp
@@ -6,9 +6,6 @@
  */
 
 #include "State.h"
-#include "Phenomenon.h"
-
-namespace state {
 
 State::State() {
 	// TODO Auto-generated constructor stub
@@ -16,26 +13,12 @@ State::State() {
 }
 
 State::~State() {
-	// TODO Auto-generated destructor stub
+	// TODO Auto-generated
+	destructor stub
 }
 
-double get_val_phen () const {
-	return val_phen_;
+void update_state () {
+    eff_state_ += i_phen_ * (val_phen_-eff_state_)
+                + i_ctrl_ * (val_ctrl_-eff_state_);
 }
 
-double get_eff_state () const {
-	return eff_state_;
-}
-
-void update_state () const {
-	val_phen_ = get_val_phen ();
-}
-
-void set_update_ctrl_state (double val_ctrl) {
-	eff_state_ = eff_state_ + (val_phen_ - eff_state_) * i_phen_ 
-				 + (val_ctrl - eff_state_) * i_ctrl_;
-
- // TOOD : this function seems to be UAF (ugly as f***) -> find an other way ?
-} 
-
-} /* namespace state */
