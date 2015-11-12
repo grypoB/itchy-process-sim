3fae42e27180dab6657e5b17b815f9de02956a00
Alexandre Devienne <grypoB@gmail.com>
2015-11-12 15:02:45 +0100

    Completed main
    Main is now actually unsing the other class : we can begin testing now
    :)

--- main.cpp
+++ main.cpp
@@ -2,9 +2,37 @@
 #include <cstdio>
 #include <iostream>
 
+#include "Simulator.h"
+#include "Controller.h"
+#include "Phenomenon.h"
+
 int main (int argc, char *argv[]) {
+    using namespace std;
+
+    std::cout << "Initialization..." << std::endl;
+
+    Server server;
+    cout << "Server OK" << endl;
+    State s(1,0,0);
+    cout << "State OK" << endl;
+    Phenomenon p(&s, -1, 100);
+    cout << "Phenomenon OK" << endl;
+    Controller c(&s, &server);
+    cout << "Controller OK" << endl;
+    Simulator sim;
+    cout << "Sim OK" << endl;
+
+    cout << endl << "Fasten yout seatbelt" << endl;
     
-    std::cout << "Hello world" << std::endl;
+    sim.addAgent(p);
+    sim.addAgent(s); // TODO invert to not have the actual value to server bu t-1
+    sim.addAgent(c);
+    sim.addAgent(server);
+
+    cout << "Here we go" << endl;
+    cout << "----------------------------------------" << endl << endl;
+
+    sim.run(10,1.1);
 
     return EXIT_SUCCESS;
 }
