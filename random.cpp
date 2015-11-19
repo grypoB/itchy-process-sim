d64d553810d3a90397239ebc571ebd4eac46acab
Alexandre Devienne <grypoB@gmail.com>
2015-11-19 18:19:03 +0100

    Style correction in random.cpp

--- random.cpp
+++ random.cpp
@@ -4,38 +4,29 @@
 #include "random.h"
 
 namespace {
-/**
- * @return random number in range (0,1]
- * More precisely [4.656612877414201268067017736029811203479766845703125e-10 , 1]
- */
-double randDouble() {
-    static bool initialized(false);
+    /**
+     * @return random number in range (0,1]
+     * More precisely [~4.656612877e-10, 1]
+     */
+    double randDouble() {
+        static bool initialized(false);
 
-    if (!initialized) {
-        srand(time(NULL)); // initialized list of random number
-        initialized = true;
+        if (!initialized) {
+            // initialized list of random number
+            std::srand(static_cast<unsigned int>(std::time(NULL)));
+            initialized = true;
+        }
+
+        // rand return int in [1, RAND_MAX-1]
+        return static_cast<double>(std::rand())/(RAND_MAX-1);
     }
-
-    // rand return int in [1, RAND_MAX-1]
-    return static_cast<double>(std::rand())/(RAND_MAX-1);
-}
 } /* anonymous namespace */
 
 namespace Rand {
-
-/** Return random number in (min,max]
- *  @param min,max if min>max, return random number in [max,min)
- */
-double rand(double min, double max) {
-    /*
-    if (min>max) {
-        double temp(min);
-        min = max;
-        max = temp;
+    /** Return random number in (min,max]
+     *  @param min,max if min>max, return random number in [max,min)
+     */
+    double rand(double min, double max) {
+        return min + randDouble()*(max-min);
     }
-    */
-
-    return min + randDouble()*(max-min);
-}
-
 } /* namespsace Rand */
