e538e4276b48d7b1e47e64e17911595b76ab744e
youpila <youpila2@gmail.com>
2015-11-24 19:58:10 +0100

    Modification of controller to secure things

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -2,8 +2,13 @@
 
 #include "Controller.h"
 
+namespace {
+    enum Agent {PHENOMENON = 0, STATE, CTRL};
+    const unsigned int NB_AGENT = 3;
+}
+
 /** Default constructor, should not be called explicitly */
-Controller::Controller() : Agent(), pState_(NULL), pServer_(NULL) {}
+Controller::Controller() : Agent(), pState_(0), pServer_(0) {}
 
 /** Create a passif controller with predefined relationship
  * @param pState the state to monitor
@@ -17,22 +22,15 @@ Controller::Controller(State* pState, Server* pServer)
 
 Controller::~Controller() {}
 
-/** Monitor the state and send data to server
+/** Monitor the state
  */
 void Controller::refresh (double) {
-    double val_phen (.0);
-    double val_state(.0);
-  
-    assert(pState_!=NULL && pServer_!=NULL);
-
-    val_phen  = pState_->get_val_phen() ;
-    val_state = pState_->get_val_state();
+    double val_state;
     
+    val_state = get_info_state();
     pState_->set_val_ctrl(val_state); // so ctrl has no effect
     
-    // TODO check server format output
-    pServer_->send("phenomenon", val_phen );
-    pServer_->send("state",      val_state);
+    transmit();
 }
 
 
@@ -41,12 +39,71 @@ void Controller::refresh (double) {
  */
 void Controller::init() {
     using namespace std;
-    vector<string> legendKeys(0,"");
+    
 
-    if(pServer_!=NULL) {
-        legendKeys.push_back("phenomenon");
-        legendKeys.push_back("state");
-
-        pServer_->introduce(legendKeys);
+    if(pServer_!= 0) {
+        if(legend_keys_.size() == 0) {
+            legend_keys_.push_back("phenomenon");
+            legend_keys_.push_back("state");
+            legend_keys_.push_back("controller");
+        }
+        
+        pServer_->introduce(legend_keys_);
     }
+}
+
+
+/** Initialize name of the agents of the simulation
+ * If not called before init or with incorrect number of arguments (3),
+ * default name will be given
+ *
+ * @param legend Vector containing the legend key which will be outputed.
+ *               The order they are in the vector is the following order:
+ *               phenomenon, state, controller
+ */
+void Controller::set_legend(std::vector<std::string> legend) {
+    using namespace std;
+    
+    if(legend_keys_.size() != NB_AGENT) {
+        legend_keys_.push_back("phenomenon");
+        legend_keys_.push_back("state");
+        legend_keys_.push_back("controller");
+    }
+            
+    else {
+        legend_keys_.push_back(legend[PHENOMENON]);
+        legend_keys_.push_back(legend[STATE]);
+        legend_keys_.push_back(legend[CTRL]);
+    }
+}
+
+/** Return the current value of the state
+ */
+double Controller::get_info_state() {
+    
+    assert(pState_!=0);
+    return pState_->get_val_state();
+}
+
+/** Send to Server the values that won't be changed
+ */
+void Controller::transmit() {
+    double val_phen (.0);
+    double val_state(.0);
+    
+    assert(pState_!=0 && pServer_!=0);
+
+    val_phen  = pState_->get_val_phen() ;
+    val_state = pState_->get_val_state();
+    
+    pServer_->send(legend_keys_[PHENOMENON], val_phen );
+    pServer_->send(legend_keys_[STATE], val_state);
+}
+
+/** Send the value of the controller to Server
+ */
+void Controller::send_val_ctrl_to_server(double val_ctrl) {
+    assert(pServer_!=0);
+    
+    pServer_->send(legend_keys_[CTRL], val_ctrl);
 }
--- src/Controller.h
+++ src/Controller.h
@@ -1,6 +1,9 @@
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
 
+#include <vector>
+#include <string>
+
 #include "State.h"
 #include "Server.h"
 
@@ -12,10 +15,18 @@ class Controller: public Agent {
 
         virtual void refresh(double time);
         virtual void init();
+        
+        void set_legend(std::vector<std::string> legend);
 
     private:
         State*  pState_; ///< State to control
         Server* pServer_; ///< Server to send data too
+        std::vector<std::string> legend_keys_; ///< Name of the agents for the server
+
+    protected:
+        double get_info_state();
+        void transmit();
+        void send_val_ctrl_to_server(double val);
 };
 
 #endif
