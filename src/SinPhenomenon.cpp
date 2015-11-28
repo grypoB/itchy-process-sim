31491caf5bc312ca8e88f727ab6f3b8a4d019017
youpila <youpila2@gmail.com>
2015-11-29 00:46:10 +0100

    Fix possiblities to divide by 0

--- src/SinPhenomenon.cpp
+++ src/SinPhenomenon.cpp
@@ -2,6 +2,7 @@
 #include "random.h"
 #include <cmath>
 #include <cfloat>
+#include <cassert>
 
 
 
@@ -20,19 +21,35 @@ namespace {
 SinPhenomenon::SinPhenomenon()
                             : Phenomenon(), period_(DEFAULT_PERIOD),
                               offset_(DEFAULT_OFFSET), ampl_(DEFAULT_AMPLITUDE),
-                              phase_(DEFAULT_PHASE) {}
+                              phase_(DEFAULT_PHASE), sat_min_(SAT_MIN), 
+                              sat_max_(SAT_MAX) {}
 
 /** Initialize a phenomenon wich create a sinusoid
  */
-SinPhenomenon::SinPhenomenon(State* pState, double period,
-                             double offset, double ampl, double phase) 
+SinPhenomenon::SinPhenomenon(State* pState, double period, double offset, 
+                             double ampl, double phase, double sat_min, 
+                             double sat_max) 
                             : Phenomenon(pState), period_(period),
-                              offset_(offset), ampl_(ampl), phase_(phase) {}
+                              offset_(offset), ampl_(ampl), phase_(phase), 
+                              sat_min_(sat_min), sat_max_(sat_max) {
+    assert(period_ > 0.);
+    assert(sat_min_ <= sat_max_);
+}
 
 SinPhenomenon::~SinPhenomenon() {}
 
 /** Return a sinusoid
  */
 double SinPhenomenon::gen_val_phen(double time) {
-    return offset_ + ampl_*sin(PI2*(time + phase_)/period_);
+    double val_phen(offset_ + ampl_*sin(PI2*(time + phase_)/period_));
+
+    if(val_phen < sat_min_) {
+        return sat_min_;
+    }
+    else if(val_phen > sat_max_) {
+        return sat_max_;
+    }
+    else {
+        return val_phen;
+    }
 }
--- src/SinPhenomenon.h
+++ src/SinPhenomenon.h
@@ -6,13 +6,16 @@
 class SinPhenomenon: public Phenomenon {
     public:
         SinPhenomenon();
-		SinPhenomenon(State* pState, double period, double offset, double ampl, double phase);
+		SinPhenomenon(State* pState, double period, double offset, double ampl, double phase,
+                      double sat_min, double sat_max);
         virtual ~SinPhenomenon();
     private:
         double period_;
         double offset_;
         double ampl_; ///< amplitude of the sinusoid
         double phase_;
+        double sat_min_; ///< lowest value the phenomenon can take
+        double sat_max_; ///< highest value the phenomenon can take
     protected:
         virtual double gen_val_phen(double time);
 };
